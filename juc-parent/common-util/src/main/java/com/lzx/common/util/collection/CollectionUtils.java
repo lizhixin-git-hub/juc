29ecad7b5b9ef136223d7d5ac9f9c35b5d5a8fea
lizhixin-git-hub <15907300798@163.com>
2021-01-08 17:22:12 +0800

    添加查找两个list中不同元素的工具方法

--- juc-parent/common-util/src/main/java/com/lzx/common/util/collection/CollectionUtils.java
+++ juc-parent/common-util/src/main/java/com/lzx/common/util/collection/CollectionUtils.java
@@ -25,4 +25,56 @@ public class CollectionUtils {
         ).collect(Collectors.toList());
     }
 
+    /**
+     * 获取两个list中的不同元素
+     *
+     * @param list1 集合1
+     * @param list2 集合2
+     * @return 不同的元素集合
+     */
+    public List<String> getDifferentElements(List<String> list1, List<String> list2) {
+        //创建存放不同元素集合
+        List<String> diff = new ArrayList<>();
+
+        //创建存储临时数据map
+        Map<String, Integer> map = new HashMap<>(list1.size() + list2.size());
+
+        //定义最大集合
+        List<String> maxList = list1;
+
+        //定义最小集合
+        List<String> minList = list2;
+
+        //获取最大集合与最小集合
+        if (list2.size() > list1.size()) {
+            maxList = list2;
+            minList = list1;
+        }
+
+        //循环最大集合
+        for (String string : maxList) {
+            map.put(string, 1);
+        }
+
+        //循环最小集合
+        for (String string : minList) {
+            Integer count = map.get(string);
+            if (count != null) {
+                map.put(string, ++count);
+                continue;
+            }
+            map.put(string, 1);
+        }
+
+        //获取不同的集合
+        for (Map.Entry<String, Integer> entry : map.entrySet()) {
+            if (entry.getValue() == 1) {
+                diff.add(entry.getKey());
+            }
+        }
+
+        //返回不同集合
+        return diff;
+    }
+
 }
